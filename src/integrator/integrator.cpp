fe3ea1ace728f43dbe9ae139ea0753ead705d773
Zichen <zw336@cornell.edu>
2023-03-30 11:27:13 -0400

    .

--- src/integrator/integrator.cpp
+++ src/integrator/integrator.cpp
@@ -21,7 +21,8 @@ SpectrumC Integrator::renderC(const Scene &scene, int sensor_id) const {
     auto end_time = high_resolution_clock::now();
     if ( scene.m_opts.log_level ) {
         std::stringstream oss;
-        oss << "Rendered in " << duration_cast<duration<double>>(end_time - start_time).count() << " seconds.";
+        oss << "Rendered in " << duration_cast<duration<double>>
+            (end_time - start_time).count() << " seconds.";
         log(oss.str().c_str());
     }
 
@@ -34,11 +35,9 @@ SpectrumD Integrator::renderD(const Scene &scene, int sensor_id) const {
     auto start_time = high_resolution_clock::now();
 
     // Interior integral
-
     SpectrumD result = __render<true>(scene, sensor_id);
 
     // Boundary integral
-
     if ( likely(scene.m_opts.sppe > 0) ) {
         render_primary_edges(scene, sensor_id, result);
     }
@@ -52,7 +51,8 @@ SpectrumD Integrator::renderD(const Scene &scene, int sensor_id) const {
     auto end_time = high_resolution_clock::now();
     if ( scene.m_opts.log_level ) {
         std::stringstream oss;
-        oss << "Rendered in " << duration_cast<duration<double>>(end_time - start_time).count() << " seconds.";
+        oss << "Rendered in " << duration_cast<duration<double>>
+            (end_time - start_time).count() << " seconds.";
         log(oss.str().c_str());
     }
 
@@ -61,28 +61,37 @@ SpectrumD Integrator::renderD(const Scene &scene, int sensor_id) const {
 
 
 template <bool ad>
-Spectrum<ad> Integrator::__render(const Scene &scene, int sensor_id) const {
+Spectrum<ad> Integrator::__render(
+    const Scene &scene, 
+    int sensor_id
+) const {
     PSDR_ASSERT_MSG(scene.is_ready(), "Input scene must be configured!");
-    PSDR_ASSERT_MSG(sensor_id >= 0 && sensor_id < scene.m_num_sensors, "Invalid sensor id!");
+    PSDR_ASSERT_MSG(sensor_id >= 0 && sensor_id < scene.m_num_sensors, 
+        "Invalid sensor id!");
 
     const RenderOption &opts = scene.m_opts;
     const int num_pixels = opts.width*opts.height;
 
     Spectrum<ad> result = zero<Spectrum<ad>>(num_pixels);
     if ( likely(opts.spp > 0) ) {
-        int64_t num_samples = static_cast<int64_t>(num_pixels)*opts.spp;
+        int64_t num_samples = static_cast<int64_t>(num_pixels) * opts.spp;
         PSDR_ASSERT(num_samples <= std::numeric_limits<int>::max());
 
         Int<ad> idx = arange<Int<ad>>(num_samples);
         if ( likely(opts.spp > 1) ) idx /= opts.spp;
 
-        Vector2f<ad> samples_base = gather<Vector2f<ad>>(meshgrid(arange<Float<ad>>(opts.width),
-                                                         arange<Float<ad>>(opts.height)),
-                                                         idx);
+        Vector2f<ad> samples_base = gather<Vector2f<ad>>(
+            meshgrid(arange<Float<ad>>(opts.width),
+                     arange<Float<ad>>(opts.height)),
+            idx);
 
-        Vector2f<ad> samples = (samples_base + scene.m_samplers[0].next_2d<ad>())
-                                /ScalarVector2f(opts.width, opts.height);
-        Ray<ad> camera_ray = scene.m_sensors[sensor_id]->sample_primary_ray(samples);
+        Vector2f<ad> samples = 
+            (samples_base + scene.m_samplers[0].next_2d<ad>()) /
+            ScalarVector2f(opts.width, opts.height);
+
+        Ray<ad> camera_ray = 
+            scene.m_sensors[sensor_id] -> sample_primary_ray(samples);
+
         Spectrum<ad> value = Li(scene, scene.m_samplers[0], camera_ray);
         masked(value, ~enoki::isfinite<Spectrum<ad>>(value)) = 0.f;
         scatter_add(result, value, idx);
@@ -95,20 +104,33 @@ Spectrum<ad> Integrator::__render(const Scene &scene, int sensor_id) const {
 }
 
 
-void Integrator::render_primary_edges(const Scene &scene, int sensor_id, SpectrumD &result) const {
+void Integrator::render_primary_edges(
+    const Scene &scene, 
+    int sensor_id, 
+    SpectrumD &result
+) const {
     const RenderOption &opts = scene.m_opts;
-
     const Sensor *sensor = scene.m_sensors[sensor_id];
-    if ( sensor->m_enable_edges ) {
-        PrimaryEdgeSample edge_samples = sensor->sample_primary_edge(scene.m_samplers[1].next_1d<false>());
+
+    if (sensor->m_enable_edges) {
+        PrimaryEdgeSample edge_samples = 
+            sensor->sample_primary_edge(scene.m_samplers[1].next_1d<false>());
         MaskC valid = (edge_samples.idx >= 0);
-#ifdef PSDR_PRIMARY_EDGE_VIS_CHECK
-        IntersectionC its = scene.ray_intersect<false>(edge_samples.ray_c);
-        valid &= ~its.is_valid();
-#endif
-        SpectrumC delta_L = Li(scene, scene.m_samplers[1], edge_samples.ray_n, valid) - 
-                            Li(scene, scene.m_samplers[1], edge_samples.ray_p, valid);
-        SpectrumD value = edge_samples.x_dot_n*SpectrumD(delta_L/edge_samples.pdf);
+
+        #ifdef PSDR_PRIMARY_EDGE_VIS_CHECK
+                IntersectionC its = scene.ray_intersect<false>(edge_samples.ray_c);
+                valid &= ~its.is_valid();
+        #endif
+
+        // delta_L is the difference of different sides of the discontinuity
+        SpectrumC delta_L = 
+            Li(scene, scene.m_samplers[1], edge_samples.ray_n, valid) - 
+            Li(scene, scene.m_samplers[1], edge_samples.ray_p, valid);
+
+        // x_dot_n is the dot product between velocity and normal direction
+        SpectrumD value = 
+            edge_samples.x_dot_n * SpectrumD(delta_L / edge_samples.pdf);
+
         masked(value, ~enoki::isfinite<SpectrumD>(value)) = 0.f;
         if ( likely(opts.sppe > 1) ) {
             value /= static_cast<float>(opts.sppe);
